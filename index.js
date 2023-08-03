f6102378b1634c02a4c7cd76b1953a6e82d4cf85
gerardo <tangogerard890@gmail.com>
2023-08-03 18:43:47 +0200

    add conditionnel

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
 let calculofRectangle = (L, l) => {
   let resolver = L + l;
-  return resolver;
+  if (resolver > 0 ){
+    return `le rectangle est egale à ${resolver }`;
+  
+  }else{
+    console.log("foul")
+  }
 
 }
 console.log(calculofRectangle(12, 6));
